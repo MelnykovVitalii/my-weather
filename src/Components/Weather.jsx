e96dfbfdf5d656d067e9b82865879a8c3c7ff3cc
Vitalii <vitaliimelnykov@gmail.com>
2021-08-15 00:56:43 +0300

    initial commit1

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,23 +1,13 @@
 import { useEffect, useState } from 'react';
 import Card from './Card';
-// import moment from 'moment';
 import './Weather.css';
 function Weather() {
 	const [error, setError] = useState(null);
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [items, setItems] = useState([]);
-	// dt: 1628966327
 
-	// console.log(moment(1628966901722));
-	// Примечание: пустой массив зависимостей [] означает, что
-	// этот useEffect будет запущен один раз
-	// аналогично componentDidMount()
 	useEffect(() => {
-		// console.log(new Date().getHours());
-		// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-		// fetch('http://api.openweathermap.org/data/2.5/weather?q=Vapnyarka,ua&appid=23d735b0cff843665f2f29f7790ab395')
-		// fetch('http://api.openweathermap.org/data/2.5/forecast?q=Vapnyarka&appid=23d735b0cff843665f2f29f7790ab395')
-		fetch('http://api.openweathermap.org/data/2.5/weather?q=Vapnyarka,ua&appid=23d735b0cff843665f2f29f7790ab395')
+		fetch('https://api.openweathermap.org/data/2.5/weather?q=Vapnyarka,ua&appid=23d735b0cff843665f2f29f7790ab395')
 			.then((res) => res.json())
 			.then(
 				(result) => {
@@ -31,8 +21,7 @@ function Weather() {
 						temp: result.main.temp,
 					});
 				},
-				// Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
-				// чтобы не перехватывать исключения из ошибок в самих компонентах.
+
 				(error) => {
 					setIsLoaded(true);
 					setError(error);
@@ -48,7 +37,6 @@ function Weather() {
 	} else {
 		return (
 			<div className="wrapper">
-				{/* <Card {...items} /> */}
 				<Card
 					city={items.city}
 					country={items.county}
